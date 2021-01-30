114aa560197d2b262833321bf2fb7cd20e4fe668
devagx <alungroome@hotmail.com>
2021-01-30 16:26:52 +0000

    Implemented exercise 002 - isFromManchester

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,18 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
+    public boolean isFromManchester(Person person) {
+        // Add your code here
+        if (person.getCity().toUpperCase().equals("MANCHESTER")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean canWatchFilm(/* Person person, int ageLimit */) {
         // Add your code here
         return false;
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
-    }
-    
 }
