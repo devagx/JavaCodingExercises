cdee09e6a0082520a40f2fee3f056f42d6b24a65
devagx <alungroome@hotmail.com>
2021-02-12 14:15:30 +0000

    Better naming of variables in Exercise001.java method countLinuxUsers

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -45,6 +45,6 @@ public class Exercise001 {
 
     public int countLinuxUsers(List<User> users) {
         // Add your code here
-         return (int) users.stream().filter(x->x.getType().toUpperCase().equals("LINUX")).count();
+         return (int) users.stream().filter(user->user.getType().toUpperCase().equals("LINUX")).count();
     }
 }
